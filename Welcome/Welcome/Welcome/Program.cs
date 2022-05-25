fda6ab202c5ab32d8e5df5094c7d4b7ef7d15d9d
Abdal Chowdhury <88795186+Abdalchy@users.noreply.github.com>
2022-05-25 16:00:00 -0400

    Welcome initial upload

---
+++ Welcome/Welcome/Welcome/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Welcome
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("welcome to Acme accounting system \nRemember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
