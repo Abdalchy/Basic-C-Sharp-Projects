593679a1affc50352b11a281fcafee8aafc3d557
Abdal Chowdhury <88795186+Abdalchy@users.noreply.github.com>
2022-05-27 16:48:36 -0400

    Initial upload

---
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class program
+    {
+        static void Main()
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 12;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(total);
+            //Console.WriteLine(otherTotal);
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int different = 10 - 5;
+            //Console.WriteLine("Ten minmus 5 = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int quotient = 100 / 17;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //double quotien = 100.0 / 17.0;
+            //Console.writeLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 10 % 2;   // comment one at a time
+            //int remainder = 11 % 2:
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.Writeline(trueOrFalse);
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 72;
+
+            //bool isWarm = currenTemperature > roomTemperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 70;
+
+            //bool isWarm = currentTemperature == roomTemperature;    //is equal to
+            //bool isWarm = currentTemperature <= roomTemperature;   //is grater or equal to
+            //bool isWarm = currentTemperature != roomTemperature;   //not equal to
+            //Console.WriteLine (isWarm)
+            //Console.ReadLine();
+        }
+    }
+}
