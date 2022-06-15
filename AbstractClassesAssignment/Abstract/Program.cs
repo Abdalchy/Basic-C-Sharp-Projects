7683dd122ce344a74c0ae503568a662cd6ccb9fe
Abdal Chowdhury <88795186+Abdalchy@users.noreply.github.com>
2022-06-15 16:51:26 -0400

    init upload

---
+++ AbstractClassesAssignment/Abstract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.EmpID = 1;
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.EmpID = 1;
+            employee.SayName();
+            Console.ReadLine();
+            employee.SayName();
+            Console.ReadLine();
+
+        
+        }
+    }
+}
