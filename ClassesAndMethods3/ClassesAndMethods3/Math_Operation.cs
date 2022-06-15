ab743ba51e7e9c1bc890696c74159ed6b9bc6a2d
Abdal Chowdhury <88795186+Abdalchy@users.noreply.github.com>
2022-06-15 16:53:51 -0400

    init upload

---
+++ ClassesAndMethods3/ClassesAndMethods3/Math_Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Methods_3
+{
+    public class Math_Operation
+    {
+        public void Passing_In_Two_Numbers(int firstInput, int secondInput)
+        {
+            int result = firstInput + 5;
+            Console.WriteLine(secondInput);
+        }
+    }
+}
