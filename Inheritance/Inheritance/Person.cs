dd4cb15b2ad76cbcee53123ab88e15f2929c5f2f
Abdal Chowdhury <88795186+Abdalchy@users.noreply.github.com>
2022-06-16 09:59:06 -0400

    init upload

---
+++ Inheritance/Inheritance/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+        public void SayName()
+        {
+            Console.WriteLine("Employee Id: " + Id + " Name: " + FirstName + " " + LastName);
+        }
+    }
+}
+
