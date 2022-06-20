714e25b4a1f1c6e4e4edd9a2c88eb87b23d7fa74
Abdal Chowdhury <88795186+Abdalchy@users.noreply.github.com>
2022-06-20 13:40:06 -0400

    init uopload

---
+++ AdditionalFeaturesAssignment/AdditionalFeaturesAssignment/Constructor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssignment
+{
+    public class Constructor
+    {
+        public Constructor() : this(10)
+        {
+        }
+        public Constructor(int Age)
+        {
+        }
+    }
+}
---
+++ AdditionalFeaturesAssignment/AdditionalFeaturesAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdditionalFeaturesAssignment
+{
+    class Program
+    {
+        const int a = 10;
+        static void Main(string[] args)
+        {
+            const int b = 20;
+            const int c = b + a;
+            Console.WriteLine(c);
+            Console.ReadLine();
+            var name = "Construct";
+        }
+    }
+}
