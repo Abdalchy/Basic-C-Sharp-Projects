dd851d8faad880b9559884141677a6868b8c40ae
Abdal Chowdhury <88795186+Abdalchy@users.noreply.github.com>
2022-06-20 13:39:08 -0400

    init upload

---
+++ ExceptionHandlingAssignment/ExceptionHandlingAssignment/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingAssignment
+{
+    public class Message
+    {
+        public static void myMethod()
+        {
+            Console.WriteLine("Please enter your age:");
+            int currentDate = DateTime.Now.Year;
+            int age = Convert.ToInt32(Console.ReadLine());
+            int DOB = currentDate - age;
+            if (age <= 0)
+            {
+                Console.WriteLine("Please enter a number greater than 0");
+                Message.myMethod();
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("You were born in " + DOB);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExceptionHandlingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Message.myMethod();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occured. Please contact your System Administrator");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
