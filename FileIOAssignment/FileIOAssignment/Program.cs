7b324cf9a6ab15743fe35481d33213519f8fb73d
Abdal Chowdhury <88795186+Abdalchy@users.noreply.github.com>
2022-06-20 13:13:15 -0400

    upload

---
+++ FileIOAssignment/FileIOAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            string num = string.Format(Console.ReadLine());
+
+            File.WriteAllText(@"C:\Logs\log.txt", num);
+
+            string display = File.ReadAllText(@"C:\Logs\log.txt");
+            Console.WriteLine(display);
+
+            Console.ReadLine();
+        }
+    }
+}
