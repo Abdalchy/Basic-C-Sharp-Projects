7da2d405843351e00a79ad0ef7300385770f5a1e
Abdal Chowdhury <88795186+Abdalchy@users.noreply.github.com>
2022-06-20 12:53:06 -0400

    upload

---
+++ StructsAssignment/StructsAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number myNumber = new Number();
+            myNumber.Amount = 52.26m;
+            Console.WriteLine(myNumber.Amount);
+            Console.ReadLine();
+        }
+    }
+}
